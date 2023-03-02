317b63050b9cfdbab8920e2bde2ead042a6e0c37
NjimsRane <njims2011@gmail.com>
2023-03-02 12:05:29 +0100

    made few changes

--- App/JS/data.js
+++ App/JS/data.js
@@ -1,39 +1,41 @@
 const newProductsData = [
     {
-        id: 1,
+        id: 'sesdddx',
         img: './IMG/shoes/s1.jpg',
         brand: 'adidas',
         desc: 'Shoes ipsum dolor sit amet consectetur adipisicing elit. In quod possimus a officia eaque vero voluptas ratione quia, quis numquam.',
         price: 45,
     },
     {
-        id: 2,
+        id: 'sasddcc',
         img: './IMG/jackets/j2.jpg',
         brand: 'D&G',
         desc: 'Jackets ipsum dolor sit amet consectetur adipisicing elit. In quod possimus a officia eaque vero voluptas ratione quia, quis numquam.',
         price: 145,
     },
     {
-        id: 3,
+        id: 'njccjsa',
         img: './IMG/pants/p2.jpg',
         brand: 'wrangler',
         desc: 'Pants ipsum dolor sit amet consectetur adipisicing elit. In quod possimus a officia eaque vero voluptas ratione quia, quis numquam.',
         price: 100,
     },
     {
-        id: 4,
+        id: 'qwueuex',
         img: './IMG/shirts/f1.jpg',
         brand: 'Hawai',
         desc: 'Shirt ipsum dolor sit amet consectetur adipisicing elit. In quod possimus a officia eaque vero voluptas ratione quia, quis numquam.',
         price: 60,
     },
     {
-        id: 5,
+        id: 'poqweex',
         img: './IMG/pull/b2.jpg',
         brand: 'chanel',
         desc: 'Pull ipsum dolor sit amet consectetur adipisicing elit. In quod possimus a officia eaque vero voluptas ratione quia, quis numquam.',
         price: 30,
     },
 ];
+
+
 
 export { newProductsData };
--- App/JS/newProducts.js
+++ App/JS/newProducts.js
@@ -7,17 +7,17 @@ function load() {
         // use array destructuring
         const { id, img, price, desc, brand } = product;
         return (
-            `<div id=product-id-${id} class="main__products-card">
+            `<div id=${id} class="main__products-card">
                     <img src=${img} alt=${brand}>
                     <div class="main__products-details">
                         <h3 class="main__products-brand">${brand}</h3>
                         <p class="main__products-description">${desc}</p>
                         <div class="main__products-purchase">
                             <p class="main__products-price">&dollar;<span> ${price}</span></p>
-                            <p id=${id} class="main__products-quantity">
-                                <button>&minus;</button>
-                                12
-                                <button>&plus;</button>
+                            <p class="main__products-quantity">
+                                <button onclick="decreament(${id})">&minus;</button>
+                                <span id=${id} class='quantity'>0</span>
+                                <button onclick="increament(${id})">&plus;</button>
                             </p>
                         </div>
                     </div>
@@ -28,6 +28,12 @@ function load() {
 
     productsContainer.innerHTML = newProduct;
 }
-
-
 load();
+
+const increament = (id) => {
+    console.log(id);
+};
+const decreament = (id) => {
+    console.log(id);
+};
+
